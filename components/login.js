084792694eaf6181a3a0783b26b24491b93c0ce5
Xuan Mai <xuanmai.k18@gmail.com>
2021-05-13 16:55:01 +0700

    change UI login

--- components/login.js
+++ components/login.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';  
 //mport { View, Text, StyleSheet, Button } from 'react-native';  
-import {Platform, StyleSheet, Text,Alert, View,TouchableOpacity,TextInput,Image,Dimensions,Button} from 'react-native'; 
+import colors from '../assets/colors'
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  Alert,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Image,
+  Dimensions,
+  Button,
+  ImageBackground
+} from 'react-native'; 
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer'
 class ViewLogin extends Component{
@@ -41,24 +54,33 @@ class ViewLogin extends Component{
         console.error(error);
     });
   }
+
   render() {
     return (
       <View style={styless.container}>
-        <Text style={{fontSize:20,color:'red'}}>Enter your username and password</Text>
-        <TextInput placeholder="Username"
-           placeholderTextColor="black"
-           underlineColorAndroid="transparent"
-           style={styless.txtInput}  onChangeText={(username) => this.setState({username:username})}/>
-        <TextInput placeholder="Password"
-            underlineColorAndroid="transparent"
-            placeholderTextColor="black"
-            secureTextEntry={true}
-            style={styless.txtInput}  onChangeText={(password) => this.setState({password:password})}/>
-        <TouchableOpacity onPress={this._onSubmit} style={styless.btnLogin}>
-            <Text style={styless.txtLogin}>Login</Text>
-        </TouchableOpacity>
+        <View style={{flex:1, alignItems:'center'}}></View>
+        <Image source={require('../assets/logo1.png')} style={{flex:2, alignSelf:'center'}}/>
+        <View style={{flex:2, alignItems:'center'}}></View>
+
+        <View style={{flex:4, alignItems: 'center'}}>
+          <Text style={{fontSize:15, color:colors.main_blue, marginTop:30, alignSelf:'center'}}>Enter your username and password</Text>
+          <TextInput placeholder="Username"
+              placeholderTextColor={colors.main_blue}
+              underlineColorAndroid="transparent"
+              style={styless.txtInput}  onChangeText={(username) => this.setState({username:username})}/>
+          <TextInput placeholder="Password"
+              placeholderTextColor={colors.main_blue}
+              underlineColorAndroid="transparent"
+              secureTextEntry={true}
+              style={styless.txtInput}  onChangeText={(password) => this.setState({password:password})}/>
+          <TouchableOpacity onPress={this._onSubmit} style={styless.btnLogin}>
+              <Text style={styless.txtLogin}>Login</Text>
+          </TouchableOpacity>
+         
+        </View>
         {/* <Button title="Go to Home"/> */}
         {/* <HomeScreen /> */}
+        
       </View>
     );
   }
@@ -68,62 +90,97 @@ const DEVICE_HEIGHT = Dimensions.get('window').height;
 const styless = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: "column"
+  },
+  image: {
+    flex: 1,
+    flexDirection: "column",
+    resizeMode: "cover",
+    justifyContent: "center",
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
   title:{
     fontSize:30,
-    color:'red'
+    color:'red',
+     textAlign: 'center',
   },
   txtInput:{
-    backgroundColor: 'rgba(0,0,0, 0.1)',
-    width: DEVICE_WIDTH - 40,
-     
-    marginHorizontal: 20,
-    padding:8,
-    borderRadius: 20,
-    color: '#000',
-    marginTop:2
+    backgroundColor: colors.white,
+    width: DEVICE_WIDTH - 60,
+    height: 50,
+
+    borderRadius:30,
+    borderWidth:0.3,
+
+    borderColor: colors.main_blue,
+    shadowColor: colors.main_blue,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.9,
+    shadowRadius: 10,
+    elevation: 5, 
+
+    alignSelf:'center',
+    color: colors.main_blue,
+    marginTop:10,
+    marginBottom:10,
+    paddingLeft:20,
+    paddingRight:20
   },
   btnLogin:{
-     width: DEVICE_WIDTH - 40,
-     backgroundColor:'rgba(0,145,234,1)',
-     padding:8,
-     borderRadius: 20,
-     marginTop:2,
- 
+    width: 182,
+    height:50,
+    backgroundColor:colors.main_blue,
+    borderRadius:30,
+
+    shadowColor: colors.main_blue,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.9,
+    shadowRadius: 1,
+    elevation: 5, 
+    alignSelf: 'center',
+    alignItems: "center",
+    marginTop:30,
   },
   txtLogin:{
     color:'#fff',
-    textAlign:'center',
-    fontSize:20
-  },
-  txtWellcom:{
-    marginTop:20,
-    fontSize:30
+    fontWeight:'700',
+    fontSize:20,
+    letterSpacing:1.5,
+    textTransform:'uppercase',
+    textAlign: 'center',
+    padding:10,
   },
   txtReg:{
-    marginBottom:30,
-    fontSize:15
-  }
+    fontSize:16,
+    color:colors.main_blue,
+    position:'absolute',
+    top:DEVICE_HEIGHT - 50
+  }, 
 });
 class Login extends Component {  
     static navigationOptions = {  
-         title: 'Login',  
+         title: '',  
+         headerShown:false,
     };  
     render() {  
-        return (  
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' },styless.container}>  
-                <Text style={styless.txtWellcom}>Welcome to our app</Text>  
-                <ViewLogin />
-                <Text style={styless.txtReg} onPress={() => this.props.navigation.navigate('Registration')}>Registration</Text>   
-            </View>  
-        );  
+        return ( 
+          // <View style={styless.container}>
+            <ImageBackground source={require('../assets/login-background.png')} style={styless.image}>
+              <ViewLogin style={{flex:1}}></ViewLogin> 
+              <Text style={{flex:1},styless.txtReg} onPress={() => this.props.navigation.navigate('Registration')}>Registration</Text>
+            </ImageBackground>
+          // </View>
+          );  
     }
 }  
 const LoginStackNavigator = createStackNavigator(  
     {  
-        LoginNavigator: Login  
+        LoginNavigator: Login 
     }  );
 export default LoginStackNavigator;
